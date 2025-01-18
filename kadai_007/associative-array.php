7ef9fe09e5a7ca04897caf6e12a209b31d0a9ecf
henmiryusei <napo.henmi@gmail.com>
2025-01-19 00:28:37 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<heml lang = "ja">
+  <head>
+    <meta charset = "utf-8">
+    <title>PHP課題7</title>
+  </head>
+  
+  <body>
+    <p>
+      <?php
+      $item = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($item);
+      ?>
+    </p>
+  </body>
+</html>
